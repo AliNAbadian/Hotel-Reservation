be2481f1458c62acd0c6e04e5f514a809092f117
Ali N. Abadian <nagshrizali@gmail.com>
2024-10-23 12:03:43 +0330

    first implemetaion

--- hotel.go
+++ hotel.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Room struct {
 	ID       int
 	Type     string
@@ -8,19 +10,43 @@ type Room struct {
 	Price    int
 }
 
+var Rooms []Room = GenerateRooms()
 
-var Rooms Room[] = 
+func main() {
 
+	input := ""
 
-func main()  {
-	
+	for input != "exit" {
+		fmt.Println("Choose an option:")
+		fmt.Println("1. Room list")
+		fmt.Println("2. Add Room")
+		fmt.Println("3. Reserve Room ")
+		fmt.Println("4. Exit")
+		fmt.Scanln(&input)
+		switch input {
+		case "1":
+			GetRoomList()
+		case "2":
+			AddRoom()
+		case "3":
+			ReserveRoom()
+		case "4":
+			fmt.Println("Exiting...")
+			return
+		default:
+			fmt.Println("Invalid option. Please try again.")
+		}
+	}
+
 }
 
+func GetRoomList() {
+
+}
 
 func AddRoom() {
 
 }
-
 
 func ReserveRoom() {
 
@@ -30,11 +56,22 @@ func CalculateRoomPrice() {
 
 }
 
-
 func GetRoomFromInput() {
 
 }
 
-func GenerateRooms() {
-	
+func GenerateRooms() []Room {
+	rooms := make([]Room, 20)
+
+	rooms = append(rooms, Room{ID: 1, Type: "Single", Status: true, BedCount: 1, Price: 100})
+	rooms = append(rooms, Room{ID: 2, Type: "Double", Status: true, BedCount: 2, Price: 200})
+	rooms = append(rooms, Room{ID: 3, Type: "Suite", Status: true, BedCount: 4, Price: 300})
+	rooms = append(rooms, Room{ID: 4, Type: "Single", Status: false, BedCount: 1, Price: 100})
+	rooms = append(rooms, Room{ID: 5, Type: "Double", Status: false, BedCount: 2, Price: 200})
+	rooms = append(rooms, Room{ID: 6, Type: "Suite", Status: false, BedCount: 4, Price: 300})
+	rooms = append(rooms, Room{ID: 7, Type: "Single", Status: true, BedCount: 1, Price: 100})
+	rooms = append(rooms, Room{ID: 8, Type: "Double", Status: true, BedCount: 2, Price: 200})
+	rooms = append(rooms, Room{ID: 9, Type: "Suite", Status: true, BedCount: 4, Price: 300})
+	rooms = append(rooms, Room{ID: 10, Type: "Single", Status: false, BedCount: 1, Price: 100})
+
 }
