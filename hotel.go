f7bd6c202e3a4a1a817f903cfeaf127563748223
Ali N. Abadian <nagshrizali@gmail.com>
2024-10-23 11:54:17 +0330

    Hotel Reserve

---
+++ hotel.go
@@ -0,0 +1,40 @@
+package main
+
+type Room struct {
+	ID       int
+	Type     string
+	Status   bool
+	BedCount int
+	Price    int
+}
+
+
+var Rooms Room[] = 
+
+
+func main()  {
+	
+}
+
+
+func AddRoom() {
+
+}
+
+
+func ReserveRoom() {
+
+}
+
+func CalculateRoomPrice() {
+
+}
+
+
+func GetRoomFromInput() {
+
+}
+
+func GenerateRooms() {
+	
+}
