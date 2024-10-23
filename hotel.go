8228a568be957efe6188af24f231cbaf8d544bd4
Ali N. Abadian <nagshrizali@gmail.com>
2024-10-23 12:17:11 +0330

    AddRooms

--- hotel.go
+++ hotel.go
@@ -32,7 +32,7 @@ func main() {
 			ReserveRoom()
 		case "4":
 			fmt.Println("Exiting...")
-			return
+			break
 		default:
 			fmt.Println("Invalid option. Please try again.")
 		}
@@ -41,11 +41,27 @@ func main() {
 }
 
 func GetRoomList() {
+	for _, room := range Rooms {
+		fmt.Printf("%+v\n", room)
+	}
+
+}
+func GetRoomFromInput() Room {
+	var room Room = Room{Status: false}
+	fmt.Println("Enter room line by line:")
+	fmt.Scanln(&room.ID)
+	fmt.Scanln(&room.Type)
+	fmt.Scanln(&room.BedCount)
+	fmt.Scanln(&room.Price)
+
+	return room
 
 }
 
 func AddRoom() {
+	room := GetRoomFromInput()
 
+	Rooms = append(Rooms, room)
 }
 
 func ReserveRoom() {
@@ -56,12 +72,30 @@ func CalculateRoomPrice() {
 
 }
 
-func GetRoomFromInput() {
-
-}
-
+// GenerateRooms creates a list of Room instances with predefined data.
+//
+// The function returns a slice of Room instances. Each Room instance has an ID, Type, Status, BedCount, and Price.
+// The Status field indicates whether the room is available (true) or not (false).
+//
+// The function initializes a slice of Room instances and appends 10 predefined Room instances to it.
+// The predefined Room instances have the following properties:
+//
+// Room ID | Type  | Status | BedCount | Price
+// --------|-------|--------|----------|------
+// 1       | Single| true   | 1        | 100
+// 2       | Double| true   | 2        | 200
+// 3       | Suite | true   | 4        | 300
+// 4       | Single| false  | 1        | 100
+// 5       | Double| false  | 2        | 200
+// 6       | Suite | false  | 4        | 300
+// 7       | Single| true   | 1        | 100
+// 8       | Double| true   | 2        | 200
+// 9       | Suite | true   | 4        | 300
+// 10      | Single| false  | 1        | 100
+//
+// The function does not take any parameters.
 func GenerateRooms() []Room {
-	rooms := make([]Room, 20)
+	rooms := []Room{}
 
 	rooms = append(rooms, Room{ID: 1, Type: "Single", Status: true, BedCount: 1, Price: 100})
 	rooms = append(rooms, Room{ID: 2, Type: "Double", Status: true, BedCount: 2, Price: 200})
@@ -74,4 +108,5 @@ func GenerateRooms() []Room {
 	rooms = append(rooms, Room{ID: 9, Type: "Suite", Status: true, BedCount: 4, Price: 300})
 	rooms = append(rooms, Room{ID: 10, Type: "Single", Status: false, BedCount: 1, Price: 100})
 
+	return rooms
 }
